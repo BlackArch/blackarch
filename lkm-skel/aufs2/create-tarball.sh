d1174193d40e6476d05b69208009d401243544eb
nopper <nopper@archpwn.org>
2010-06-23 18:53:57 +0200

    Sync lkm-skel/aufs2 with ABS

--- lkm-skel/aufs2/create-tarball.sh
+++ lkm-skel/aufs2/create-tarball.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-AUFS2VERSION=""
-KERNELVERSION=2.6.31
-GITSNAPSHOT=20090910
+AUFS2VERSION="-34"
+KERNELVERSION=2.6.34
+GITSNAPSHOT=20100517
 # aufs2 (no -xx) for the latest -rc version.
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
 cd aufs2-standalone.git
-git checkout origin/aufs2${AUFS2VERSION}
+#git checkout origin/aufs2${AUFS2VERSION}
+git checkout origin/aufs2
 #*** apply "aufs2-base.patch" and "aufs2-standalone.patch" to your kernel source files.
 cd ..
 rm -rf aufs2-${KERNELVERSIN}_${GITSNAPSHOT}
