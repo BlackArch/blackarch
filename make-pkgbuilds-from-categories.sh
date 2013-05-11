e26483a214fa74e38d6db86dfa7c5e6ab298afae
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-11 09:05:04 -0400

    Finished pkgbuild generating script.

---
+++ make-pkgbuilds-from-categories.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+for cat in categories/* ; do
+(
+cat <<EOF
+pkgname=archtrack-$cat
+pkgver=0.0
+pkgrel=0
+pkgdesc="security pseudo-package"
+arch=('any')
+url="http://github.com/EvanTeitelman/archtrack"
+license=()
+groups=()
+EOF
+
+echo -e "depends=(\n$(< $cat)\n)"
+
+cat <<EOF
+makedepends=()
+optdepends=()
+provides=()
+conflicts=()
+replaces=()
+backup=()
+options=()
+install=
+changelog=
+source=()
+noextract=()
+md5sums=()
+EOF
+
+cat <<\EOF
+#build() {
+#  cd "$srcdir/$pkgname-$pkgver"
+#
+#  ./configure --prefix=/usr
+#  make
+#}
+
+#package() {
+#  cd "$srcdir/$pkgname-$pkgver"
+#
+#  make DESTDIR="$pkgdir/" install
+#}
+EOF
+) >> pkgbuilds/PKGBUILD-$(basename $cat)
+done
