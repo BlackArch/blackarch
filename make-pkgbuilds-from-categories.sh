e671c1d0b83a4f34a51f2d10b3790a96c88ddcf2
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-11 09:41:16 -0400

    Change PKGBUILD licenses and versions.
    This makes the PKGBUILD versions 0.1 and sets the licenses to 'Various'.

--- make-pkgbuilds-from-categories.sh
+++ make-pkgbuilds-from-categories.sh
@@ -4,12 +4,12 @@ for cat in categories/* ; do
 (
 cat <<EOF
 pkgname=archtrack-$(basename $cat)
-pkgver=0.0
+pkgver=0.1
 pkgrel=0
 pkgdesc="security pseudo-package"
 arch=('any')
 url="http://github.com/EvanTeitelman/archtrack"
-license=()
+license=('Various')
 groups=()
 EOF
 
