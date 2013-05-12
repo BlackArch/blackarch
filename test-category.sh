c7f934e136067814d18b35f6e36ab66354f929fc
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-12 08:13:38 -0400

    Extend functionality of test-category script.
    This adds multi-category support and an already-installed check to
    test-category.

--- test-category.sh
+++ test-category.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-grep -v '^#' "$1" |
-while read package ; do
-	yaourt --noconfirm -S $package
-	sudo pacman -D $package --asdep
-done 
+set -e
+
+for arg ; do
+	echo "Checking '$arg'..."
+	grep -v '^#' "$arg" |
+	while read package ; do
+		if ! yaourt -Qi $package &>/dev/null ; then
+			yaourt --noconfirm -S $package
+			sudo pacman -D $package --asdep
+		else
+			echo "'$package' already installed."
+		fi
+	done 
+done
