a6bc01a5928869791044919d5fedf113637213e0
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-12 20:46:20 -0400

    Increment version number.

--- generate-pkgbuilds.sh
+++ generate-pkgbuilds.sh
@@ -9,7 +9,7 @@ for cat in categories/* ; do
 cat <<EOF
 # Maintainer: Evan Teitelman <teitelmanevan at gmail dot com>
 pkgname=archtrack-$(basename $cat)
-pkgver=0.2
+pkgver=0.3
 pkgrel=1
 pkgdesc="This pseudo-package is part of Archtrack, a project inspired by Backtrack and Kali. Archtrack is able to turn an Arch Linux system into a fully functional penetration testing system."
 arch=('any')
