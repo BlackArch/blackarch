653cfbf229278226ef918050abcbd7fb6b474849
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-19 14:40:47 -0400

    Update 'check-templates'.
    This updates the package directory and template filename. This also
    makes the script check to see if the template file exists before trying
    the other checks.

--- scripts/check-templates.sh
+++ scripts/check-templates.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-for package in aur-packages/* ; do
-	pkgbuild=$package/PKGBUILD
+for package in ../packages/* ; do
+	pkgbuild=$package/PKGBUILD.in
+
+	if ! [[ -f $pkgbuild ]] ; then
+		echo >&2 $package missing entirely
+		continue
+	fi
+
 	if ! grep -q '%PKGDESC%' $pkgbuild ; then
-		echo $package "missing desc"
+		echo $package is missing desc
 	fi
 	if ! grep -q '%PKGNAME%' $pkgbuild ; then
-		echo $package "missing name"
+		echo $package is missing name
 	fi
-	if ! grep -q '%CATEGORIES%' $pkgbuild ; then
-		echo $package "Missing cats"
+	if ! grep -q '%GROUPS%' $pkgbuild ; then
+		echo $package is missing groups
 	fi
 done
