227c49de844af7d4de9502d13499c00048b454a3
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-19 19:08:57 -0400

    Update 'make-info' script.
    This updates the 'make-info' script with the new field placeholders,
    file names, etc. This also adds a PKGBUILD existence check.

--- scripts/make-info.sh
+++ scripts/make-info.sh
@@ -1,46 +1,56 @@
 #!/bin/bash
 
 #for package in aur-packages/dnsmap ; do
-for package in aur-packages/* ; do
+for package in ../packages/* ; do
 	pkgbuild=$package/PKGBUILD
-	# categories               the package's archtrack categories
-	categories=(archtrack $(egrep "^#?$(basename $package)" pseudo-dependency-lists/* |\
-	cut -d':' -f1 | cut -d'/' -f2))
+
+	# Skip if PKGBUILD doesn't exist.
+	if ! [[ -f $pkgbuild ]] ; then
+		continue
+	fi
+
+	# upstream repo
+	upstream_repo=$(grep "^$(basename $package)\s" ../package-repos | cut -d' ' -f2)
+
+	# groups               the package's archtrack groups
+	groups=(archtrack $(egrep "^#?$(basename $package)" ../pseudo-dependency-lists/* |\
+	cut -d':' -f1 | cut -d'/' -f3))
 
 	# description              a description of the package
 	description=$(grep '^pkgdesc' $pkgbuild | cut -d'=' -f2)
 
 	# aur-package-name         if applicable, the name of the package on the aur
-	aur_package_name=$(basename $package)
+	upstream_package_name=$(basename $package)
 
 	# name                     the official/pretty name of the package or primary tool associated with the package
 	# we need to make these manually :/
-	name=$aur_package_name
+	name=$upstream_package_name
 
 	# aur-maintainer           if applicable, the aur maintainer
-	aur_maintainer=$(grep -i '#.*maintainer' $pkgbuild | head -n1 | sed -r 's/^.*:\s?//')
-	if [[ -z $aur_maintainer ]] ; then
-		aur_maintainer=$(grep -i '#.*contributor' $pkgbuild | head -n1 | sed -r 's/^.*:\s?//')
+	upstream_maintainer='"'$(grep -i '#.*maintainer' $pkgbuild | head -n1 | sed -r 's/^.*:\s?//')'"'
+	if [[ -z $upstream_maintainer ]] ; then
+		upstream_maintainer='"'$(grep -i '#.*contributor' $pkgbuild | head -n1 | sed -r 's/^.*:\s?//')'"'
 	fi
 
 	# working                   describes the status of the package (working, not working)
-	if grep -q "#$(basename $package)" pseudo-dependency-lists/* ; then
+	if grep -q "#$(basename $package)" ../pseudo-dependency-lists/* ; then
 		working=false
 	else
 		working=true
 	fi
 
 	# aur-working               describes the status of the package on the AUR (working, not working)
-	aur_working=$working
+	upstream_working=$working
 
 	#cat <<EOF
 	cat > $package/info <<EOF
 pretty_name="${name}"
-aur_name="${aur_package_name}"
-aur_maintainer="${aur_maintainer}"
-categories=(${categories[@]})
+upstream_name="${upstream_package_name}"
+upstream_maintainer=${upstream_maintainer}
+groups=(${groups[@]})
 description=${description}
 working=${working}
-aur_working=${aur_working}
+upstream_working=${upstream_working}
+upstream_repo=${upstream_repo}
 EOF
 done
