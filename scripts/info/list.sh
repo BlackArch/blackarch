ed7757648457e53360c21de93706ea8a9650677e
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-21 16:37:47 -0400

    Add 'list' script.
    This script provides useful lists of packages, groups, etc.

---
+++ scripts/info/list.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Provides some informative lists.
+
+usage() {
+	echo >&2 "Help will come soon. For now, look at the source."
+}
+
+cd "$(dirname "$0")/../../packages"
+case "$1" in
+	p|packages)
+		ls -1
+		;;
+	w|working)
+		for package in * ; do
+			grep -q '^working=true' "$package/info" && echo "$package"
+		done
+		;;
+	b|broken)
+		for package in * ; do
+			grep -q '^working=false' "$package/info" && echo "$package"
+		done
+		;;
+	u|upstream-broken)
+		for package in * ; do
+			grep -q '^upstream_working=false' "$package/info" && echo "$package"
+		done
+		;;
+	t|to-find)
+		ls -1 ../research/kali-menu/items-tofind
+		;;
+	a|all-groups)
+		for package in * ; do
+			source "$package/info"
+			for group in ${groups[@]} ; do
+				echo $group
+			done
+		done | sort -u
+		;;
+	g|groupings)
+		for package in * ; do
+			source "$package/info"
+			for group in ${groups[@]} ; do
+				[[ $group = archtrack ]] || echo "$group $package"
+			done
+		done | sort
+		;;
+	*)
+		usage
+		exit 1
+		;;
+esac
