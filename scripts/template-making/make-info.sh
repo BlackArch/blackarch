ccdd0909d2215bc231ada2a7e7fed240e28c02d5
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-23 12:29:00 -0400

    Make the 'make-info' script prompt the user for package groups.

--- scripts/template-making/make-info.sh
+++ scripts/template-making/make-info.sh
@@ -21,9 +21,13 @@ echo "$package"
 
 	upstream_repo=$(yaourt -Si $upstream_package_name | sed '/^Repository/s/^.*: //')
 
-	groups=($(grep -E "^#?$(basename $package)" ../pseudo-dependency-lists/* | \
-	          cut -d':' -f1 | cut -d'/' -f3))
+	#groups=($(grep -E "^#?$(basename $package)" ../pseudo-dependency-lists/* | \
+	#          cut -d':' -f1 | cut -d'/' -f3))
 
+	read -p "Groups: " read_groups
+	groups=($read_groups)
+
+	# Add the 'archtrack' group.
 	if (( ${#groups[@]} != 0 )) ; then
 		groups=(archtrack ${groups[@]})
 	else
