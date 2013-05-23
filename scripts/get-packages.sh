f8c7e2cd7d54c7d2acb3d5d5d0041a13690fe2cc
Evan Teitelman <teitelmanevan@gmail.com>
2013-05-22 20:12:37 -0400

    Make 'get-packages' more intuitive.
    This adds more descriptive output and removes some clutter.

--- scripts/get-packages.sh
+++ scripts/get-packages.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Gets package source archives from the AUR or ABS.
 
-root="$(dirname "$0")/../.."
+root="$(dirname "$0")/.."
 get_dir="$root/new-packages"
+[[ -d "$get_dir" ]] || mkdir -p "$get_dir"
+cd "$get_dir" || exit 1
 
-echo "Saving packages to '$get_dir'..."
+echo "Saving packages to '$get_dir'."
 
 for package ; do
-	packer -G $(basename "$package") ||
-	{ abs $package;
-	  cp -nr /var/abs/*/$package "$get_dir" ||
-     echo "Could not find '$package'."; }
+	packer -G $package &>/dev/null &&
+	echo "Got '$package' from aur." ||
+	{ cp -nr /var/abs/*/$package . 2>/dev/null ||
+     echo "Could not find '$package'." &&
+     echo "Got '$package' from abs."; }
 done
