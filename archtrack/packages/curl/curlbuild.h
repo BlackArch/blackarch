660752884a3a95bebd9d95c219878af1fb9282cc
Evan Teitelman <teitelmanevan@gmail.com>
2013-07-16 11:33:09 -0400

    archtrack: Remove official packages.

--- archtrack/packages/curl/curlbuild.h
+++
@@ -1,9 +0,0 @@
-#include <bits/wordsize.h>
-
-#if __WORDSIZE == 32
-#include "curlbuild-32.h"
-#elif __WORDSIZE == 64
-#include "curlbuild-64.h"
-#else
-#error "Unknown word size"
-#endif
--- archtrack/packages/llvm/llvm-Config-llvm-config.h
+++
@@ -1,9 +0,0 @@
-#include <bits/wordsize.h>
-
-#if __WORDSIZE == 32
-#include "llvm-config-32.h"
-#elif __WORDSIZE == 64
-#include "llvm-config-64.h"
-#else
-#error "Unknown word size"
-#endif
