0466f13a5ac487faf68455594a7fc770c8b14fd5
Eduard Toloza <tolosaeduard@gmail.com>
2018-09-30 07:21:59 -0500

    Added wscript tool
    Fix #1717

---
+++ updateb.sh
@@ -0,0 +1,10 @@
+git fetch upstream
+
+# Make sure that you're on your master branch:
+
+git checkout master
+
+# Rewrite your master branch so that any commits of yours that
+# aren't already in upstream/master are replayed on top of that
+# other branch:
+git merge upstream/master
