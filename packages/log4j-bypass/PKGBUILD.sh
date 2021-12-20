48baa25d459a8ef41b5460147f4ca7e17588d02f
cyberqueen-meg <62813469+cyberqueen-meg@users.noreply.github.com>
2021-12-20 07:19:45 -0700

    Update PKGBUILD.sh

--- packages/log4j-bypass/PKGBUILD.sh
+++ packages/log4j-bypass/PKGBUILD.sh
@@ -1,14 +1,35 @@
+# This file is part of BlackArch Linux ( https://www.blackarch.org/ ).
+# See COPYING for license details.
+
 pkgname='log4j-bypass'
 pkgver='0.1.1'
 pkgrel='0.1.1'
 pkgdesc='Log4j web app tester that includes WAF bypasses'
 arch=('any')
+groups=('blackarch' 'blackarch-webapp' 'blackarch-fuzzer' 'blackarch-scanner')
 url='https://github.com/cyberqueen-meg/log4j-bypass'
 license=('MIT')
-groups=('blackarch' 'blackarch-webapp' 'blackarch-fuzzer' 'blackarch-scanner')
-depends=('python3')
+depends=('python)
 makedepends=('git')
 source=("git+https://github.com/cyberqueen-meg/$pkgname.git")
+sha512sums=('SKIP')
+
+pkgver() {
+  cd $pkgname
+
+  # use, if no version string provided neither in sources nor in git describe:
+  echo $(git rev-list --count HEAD).$(git rev-parse --short HEAD)
+
+  # if tag exists, use this
+  git describe --long --tags | sed 's/\([^-]*-g\)/r\1/;s/-/./g'
+}
+
+prepare() {
+  cd $pkgname
+
+  ...
+}
+
 
 build () {
     git clone $source
