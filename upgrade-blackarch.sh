11b3704fd3ac69b875b818cb9ccf9a9540950808
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2024-12-30 00:46:57 -0600

    Fixed some isses

--- upgrade-blackarch.sh
+++ upgrade-blackarch.sh
@@ -5,5 +5,8 @@
 
 sudo pacman -Rdd jre-openjdk jdk-openjdk jre11-openjdk jre11-openjdk-headless calamares python-uvicorn
 sudo pacman -S archlinux-keyring
-sudo pacman -Syu
+sudo pacman -S blackarch-keyring
+sudo pacman -S blackarch-mirrorlist
+sudo pacman -Syy
+sudo pacman -Syu --no-confirm --disable-download-timeout
 
