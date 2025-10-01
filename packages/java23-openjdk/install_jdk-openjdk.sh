8580c2e0bdeb1d2feb78dc9e60fddb06012a2dbc
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2025-10-01 20:07:42 +0200

    java23-openjdk: initial commit.

---
+++ packages/java23-openjdk/install_jdk-openjdk.sh
@@ -0,0 +1,50 @@
+THIS_JDK='java-23-openjdk'
+
+fix_default() {
+  if [ ! -x /usr/bin/java ]; then
+    /usr/bin/archlinux-java unset
+    echo ""
+  else
+    /usr/bin/archlinux-java get
+  fi
+}
+
+post_install() {
+  default=$(fix_default)
+  case ${default} in
+    "")
+      /usr/bin/archlinux-java set ${THIS_JDK}
+      ;;
+    ${THIS_JDK})
+      # Nothing
+      ;;
+    *)
+      echo "Default Java environment is already set to '${default}'"
+      echo "See 'archlinux-java help' to change it"
+      ;;
+  esac
+
+  if [ ! -f /etc/ssl/certs/java/cacerts ]; then
+    /usr/bin/update-ca-trust
+  fi
+}
+
+post_upgrade() {
+  default=$(fix_default)
+  if [ -z "${default}" ]; then
+    /usr/bin/archlinux-java set ${THIS_JDK}
+  fi
+
+  if [ ! -f /etc/ssl/certs/java/cacerts ]; then
+    /usr/bin/update-ca-trust
+  fi
+}
+
+pre_remove() {
+  if [ "x$(fix_default)" = "x${THIS_JDK}" ]; then
+    # Check JRE is still available
+    if [ -x /usr/lib/jvm/${THIS_JDK}/bin/java ]; then
+      /usr/bin/archlinux-java unset
+    fi
+  fi
+}
