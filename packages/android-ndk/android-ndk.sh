27877cdc9080dc3d847f6a40d89c3f8b313b632d
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2025-11-25 00:31:20 +0100

    to-release: released packages.

--- packages/android-ndk/android-ndk.sh
+++ packages/android-ndk/android-ndk.sh
@@ -1,4 +1,6 @@
-export PATH=$PATH:/opt/android-ndk
+export PATH="$PATH:/opt/android-ndk"
 export ANDROID_NDK=/opt/android-ndk
 # Some programs such as gradle ask this as well:
 export ANDROID_NDK_HOME=/opt/android-ndk
+# Some other programs like Godot wants this:
+export ANDROID_NDK_ROOT=/opt/android-ndk
